3743ede8bb3c6b1ae43c4c0b1f527155e76beb1e
urosmeh <uros.meh@gmail.com>
2024-11-07 19:15:24 +0100

    feat: scss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
-import { Heading } from '@chakra-ui/react';
-import classes from './App.module.css';
+import { Heading, Box } from '@chakra-ui/react';
+import classes from './App.module.scss';
 
 function App() {
   return (
     <div className={classes.app}>
       <Heading as={'h1'}>Welcome to XYZ</Heading>
       <Heading as={'h6'}>Start by uploading document!</Heading>
+      <Box></Box>
     </div>
   );
 }
