8c9f83d3240a2bb4a37d38eea8f341ef1ed30c3c
urosmeh <uros.meh@gmail.com>
2024-11-07 14:08:33 +0100

    feat: chakra install, app layout & removal of unnecessary code

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ChakraProvider value={defaultSystem}>
+      <App />
+    </ChakraProvider>
   </StrictMode>,
-)
+);
