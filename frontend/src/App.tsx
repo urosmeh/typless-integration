1d72b67ba14e588717be517aefc52140daf1a1ad
urosmeh <uros.meh@gmail.com>
2024-11-08 09:30:04 +0100

    feat: drag and drop file upload

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,40 @@
-import { Heading, Box } from '@chakra-ui/react';
+import { Box, Button, Heading, IconButton } from '@chakra-ui/react';
 import classes from './App.module.scss';
+import DragAndDrop from './components/DragAndDrop/DragAndDrop';
+import { useCallback, useState } from 'react';
+import { FileWithPath } from 'react-dropzone';
+import { MdClose } from 'react-icons/md';
 
 function App() {
+  const [uploadedFile, setUploadedFile] = useState<FileWithPath | null>(null);
+
+  const onRemoveFile = useCallback(() => {
+    setUploadedFile(null);
+  }, [setUploadedFile]);
+
   return (
     <div className={classes.app}>
       <Heading as={'h1'}>Welcome to XYZ</Heading>
       <Heading as={'h6'}>Start by uploading document!</Heading>
-      <Box></Box>
+      {!uploadedFile ? (
+        <DragAndDrop setUploadedFile={setUploadedFile} />
+      ) : (
+        <Box className={classes.filePreview}>
+          <IconButton onClick={onRemoveFile} variant={'outline'} size={'xs'}>
+            <MdClose />
+          </IconButton>
+          <iframe height="100%" src={URL.createObjectURL(uploadedFile)} />
+          <Button
+            className={classes.processButton}
+            disabled={!uploadedFile}
+            onClick={() => {
+              console.log('process file');
+            }}
+          >
+            Process
+          </Button>
+        </Box>
+      )}
     </div>
   );
 }
---
+++ frontend/src/components/DragAndDrop/DragAndDrop.tsx
@@ -0,0 +1,47 @@
+import { Box, Text } from '@chakra-ui/react';
+import * as reactDropzone from 'react-dropzone';
+const { useDropzone } = reactDropzone;
+import classes from './DragAndDrop.module.scss';
+import classNames from 'classnames';
+import { useCallback } from 'react';
+import { FileWithPath } from 'react-dropzone';
+
+type DragAndDropProps = {
+  setUploadedFile: (file: FileWithPath | null) => void;
+};
+
+const DragAndDrop = ({ setUploadedFile }: DragAndDropProps) => {
+  const onDrop = useCallback(
+    (acceptedFiles: FileWithPath[]) => {
+      if (acceptedFiles.length > 0) {
+        setUploadedFile(acceptedFiles[0]);
+      }
+    },
+    [setUploadedFile],
+  );
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: {
+      'application/pdf': ['.pdf'],
+    },
+  });
+
+  return (
+    <Box
+      {...getRootProps()}
+      className={classNames(classes.container, {
+        [classes.isDragActive]: isDragActive,
+      })}
+    >
+      <input {...getInputProps()} accept="application/pdf" />
+      {isDragActive ? (
+        <Text>Drop to upload.</Text>
+      ) : (
+        <Text>Drag files or click here to upload</Text>
+      )}
+    </Box>
+  );
+};
+
+export default DragAndDrop;
